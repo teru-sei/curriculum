724f8ea6be85b8c40ec17fee9de509dfa5de49a4
hfc.3591@gmail.com <85048706+teru-sei@users.noreply.github.com>
2021-09-15 19:43:31 +0900

    2-6

--- JavaStudy/2-6/src/study/Study.java
+++ JavaStudy/2-6/src/study/Study.java
@@ -21,24 +21,26 @@ public class Study {
         Date now = new Date();
 
         // 問① ローカル変数「now」を使用し、本日の日付を表示してください。
-
+        System.out.println(now);
 
         // Calendarクラスのインスタンスを生成
         Calendar calendar = Calendar.getInstance();
 
         // 問② 「(Calendar.MONTH) + 数値」の処理は、想定していない値となる可能性があります。
         // その理由をコメントへ記述してください。
-        // [ここへ記述]
+        // [12の+1が1月ではなく13月になるため]
         System.out.println(calendar.get(Calendar.YEAR));
         System.out.println(calendar.get(Calendar.MONTH) + 1);
         System.out.println(calendar.get(Calendar.DATE));
         // 問③ 上記の「calendar」を使用し、本日から1年2ヶ月15日先の日付を表示しなさい。
         // - この課題に関しては課題実施日によって結果が異なりますので、Wiki課題の画像とは結果が異なります。
         // - また、うるう年の場合は設定内容が正しくとも日付がズレて表示される可能性があります。
-
-
-
-
+        calendar.add(Calendar.YEAR, 1);
+        calendar.add(Calendar.MONTH, 2);
+        calendar.add(Calendar.DATE, 15);
+        System.out.println(calendar.get(Calendar.YEAR));
+        System.out.println(calendar.get(Calendar.MONTH) + 1);
+        System.out.println(calendar.get(Calendar.DATE));
 
 
         // うるう年判定
@@ -51,6 +53,14 @@ public class Study {
      * @param year
      */
     private static void printLeapYear(final int year) {
+    	boolean IsleapYear = false;
+    	if (year % 4 == 0) {
+    		if((year % 100) == 0 ) {
+    			IsleapYear = ((year % 400) == 0);
+    		} else {
+    			IsleapYear = true;
+    		}
+    	}
         System.out.println(year + "年は、うるう年" + (Year.isLeap(year) ? "です。" : "ではありません。"));
     }
 }
