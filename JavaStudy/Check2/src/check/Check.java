326d2610551151e7773eabfa64fc244465900b9d
hfc.3591@gmail.com <85048706+teru-sei@users.noreply.github.com>
2021-09-16 11:39:46 +0900

    Check2

---
+++ JavaStudy/Check2/src/check/Check.java
@@ -0,0 +1,20 @@
+package check;
+
+import constants.Constants;
+
+public class Check {
+	
+	private static String firstName = "井上";
+	private static String lastName = "輝成";
+	private static String printName = firstName + lastName;
+	
+	static Pet pet = new Pet(Constants.CHECK_CLASS_JAVA, Constants.CHECK_CLASS_HOGE);
+	static RobotPet rpet = new RobotPet(Constants.CHECK_CLASS_R2D2, Constants.CHECK_CLASS_LUKE);
+	
+	
+	public static void main(String[] args) {
+		System.out.println("printNameメソッド → " + printName);
+		pet.introduce();
+		rpet.introduce();
+	}
+}
