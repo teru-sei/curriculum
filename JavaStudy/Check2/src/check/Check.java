a24e80a66798e9dfa3ce16cae152f64dd16fd002
hfc.3591@gmail.com <85048706+teru-sei@users.noreply.github.com>
2021-09-17 07:26:36 +0900

    printNameメソッド

--- JavaStudy/Check2/src/check/Check.java
+++ JavaStudy/Check2/src/check/Check.java
@@ -6,14 +6,17 @@ public class Check {
 	
 	private static String firstName = "井上";
 	private static String lastName = "輝成";
-	private static String printName = firstName + lastName;
+	
+	private static String printName(String firstName, String lastName) {
+		return firstName + lastName;
+	}
 	
 	static Pet pet = new Pet(Constants.CHECK_CLASS_JAVA, Constants.CHECK_CLASS_HOGE);
 	static RobotPet rpet = new RobotPet(Constants.CHECK_CLASS_R2D2, Constants.CHECK_CLASS_LUKE);
 	
 	
 	public static void main(String[] args) {
-		System.out.println("printNameメソッド → " + printName);
+		System.out.println("printNameメソッド → " + printName(firstName,lastName));
 		pet.introduce();
 		rpet.introduce();
 	}
