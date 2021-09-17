86879e46c4e919b2594cdaf30f8311c2ca6e52ee
hfc.3591@gmail.com <85048706+teru-sei@users.noreply.github.com>
2021-09-18 06:36:53 +0900

    Check2 記述箇所修正

--- JavaStudy/Check2/src/check/Check.java
+++ JavaStudy/Check2/src/check/Check.java
@@ -11,11 +11,11 @@ public class Check {
 		return firstName + lastName;
 	}
 	
-	static Pet pet = new Pet(Constants.CHECK_CLASS_JAVA, Constants.CHECK_CLASS_HOGE);
-	static RobotPet rpet = new RobotPet(Constants.CHECK_CLASS_R2D2, Constants.CHECK_CLASS_LUKE);
-	
-	
 	public static void main(String[] args) {
+		
+		Pet pet = new Pet(Constants.CHECK_CLASS_JAVA, Constants.CHECK_CLASS_HOGE);
+		RobotPet rpet = new RobotPet(Constants.CHECK_CLASS_R2D2, Constants.CHECK_CLASS_LUKE);
+		
 		System.out.println("printNameメソッド → " + printName(firstName,lastName));
 		pet.introduce();
 		rpet.introduce();
