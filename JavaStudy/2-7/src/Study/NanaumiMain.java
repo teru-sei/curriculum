1eaff67d8da1d47a98ec73162aa2e6964d92a17d
hfc.3591@gmail.com <85048706+teru-sei@users.noreply.github.com>
2021-09-28 16:18:21 +0900

    練習

---
+++ JavaStudy/2-7/src/Study/NanaumiMain.java
@@ -0,0 +1,24 @@
+package Study;
+
+/**
+ * 3. 七海クラス: メイン処理
+ * <pre>インターフェースを実装したクラスのインスタンス生成してメソッドを呼び出す<pre>
+ */
+public class NanaumiMain {
+ 
+ public static void main(String[] args) {
+  // 3. 現実（1, 2 を使ってみる）
+ Nanaumi Nanaumi = new Nanaumi("七海", "2019/03");
+ Nanaumi.daseyaKinmuhyo();
+ Nanaumi.daseyaKotsuhi();
+ Nanaumi.doNothing();
+ Nanaumi.goToSevenEleven();
+ 
+	  // 3. 現実（1, 2 を使ってみる）
+	 Nanaumi nanaumi = new Nanaumi("七海", "2019/01");
+	 
+	  // Javaカリキュラム作って報告
+	 final String message = nanaumi.doCreateJavaCurriculum();
+	 System.out.println("message = " + message);
+	 }
+}
