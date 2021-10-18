64798703140b7cf211a611a060d364fdb89025f5
hfc.3591@gmail.com <85048706+teru-sei@users.noreply.github.com>
2021-10-19 08:37:15 +0900

    4-2

--- JavaStudy/4-2/SelectServlet.java
+++ JavaStudy/4-2/SelectServlet.java
@@ -4,6 +4,11 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.io.PrintWriter;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 /**
  * サーブレット
  * HTMLから情報を受け取り、表示させる処理
@@ -16,10 +21,10 @@ public class SelectServlet extends HttpServlet {
             throws IOException, ServletException {
         request.setCharacterEncoding("UTF-8");
         // 問① select.htmlから渡された値を受け取るために下記を修正しましょう。
-        String value = request.getParameter("ここを修正");
+        String value = request.getParameter("month");
         response.setContentType("text/html; charset=UTF-8");
         PrintWriter out = response.getWriter();
         // 問② エビデンスと同じ表示になるように修正しましょう。
-        out.println("ここを修正");
+        out.println(request.getParameter("今は" + month + "です。"));
     }
 }
