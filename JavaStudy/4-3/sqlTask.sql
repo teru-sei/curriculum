18550d367575109217952de997352383deb74323
hfc.3591@gmail.com <85048706+teru-sei@users.noreply.github.com>
2021-10-27 09:10:28 +0900

    4-3

--- JavaStudy/4-3/sqlTask.sql
+++ JavaStudy/4-3/sqlTask.sql
@@ -1,33 +1,54 @@
 /* ■ [回答]と記載のある箇所へ、1〜8の各課題内容に沿ったSQL文を記述しなさい。 */
  
 -- 1. 店舗テーブルから店舗名を抽出しなさい。また、列名の表示は別名で'店舗名'とすること。
--- SELECT store_name AS 店舗名 FROM store_table
+SELECT store_name AS 店舗名 FROM store_table
 https://gyazo.com/4418ae12faf75243485e06ecd75370ba
  
  
 -- 2. 店舗情報を店舗名のABC順に抽出しなさい。
--- [回答]
+SELECT * FROM store_table ORDER BY store_nameAbc ASC
+https://gyazo.com/c23497115e2e6bf9bdad43a86d83b1e6
  
  
 -- 3. 在庫テーブルに店舗テーブル、商品テーブルを「内部結合」し、店舗名・商品名・在庫数を全て取得しなさい。
--- [回答]
+SELECT * FROM stock_table AS stock
+JOIN store_table AS store ON stock.store_code = store.store_code
+JOIN goods_table AS goods ON stock.goods_code = goods.goods_code
+https://gyazo.com/5894701c4b78b2a12b5dc33b94017233
  
  
 -- 4. 商品テーブルから全商品の単価の平均値を抽出しなさい。
--- [回答]
+SELECT AVG(price) FROM goods_table
+https://gyazo.com/a8a5cd2b0b3d96120139cdd7e01aa13d
  
  
 -- 5. 店舗コード='EA01'の在庫数の平均値より大きい在庫数を持つ店舗コードを抽出しなさい。
--- [回答]
+SELECT store_code, AVG(quantity) FROM stock_table
+GROUP BY store_code HAVING AVG(quantity) >
+(SELECT AVG(quantity) FROM stock_table
+WHERE store_code = 'EA01')
+https://gyazo.com/f9c17f0d630413be8995602932251b0e
  
  
 -- 6. 商品テーブルに「商品コード='M001'、商品名='マフラー'、単価=4500円、更新日付=本日日付」のデータを追加しなさい。※実行後のSELECT結果も貼付すること。
--- [回答]
- 
+INSERT INTO goods_table
+(goods_code, goods_name, price, update_day)
+VALUES('M001', 'マフラー', '4500', '2021-10-26')
+ https://gyazo.com/e5b725c0f410241d1d5ece59368e2104
+
  
 -- 7. 在庫テーブルの商品コード='S987'、かつ、店舗コード='EA01'に対して、「在庫数=10、更新日付=本日日付」で更新しなさい。※実行後のSELECT結果も貼付すること。
--- [回答]
+UPDATE stock_table
+SET
+quantity = 10,
+update_day = '2021-10-27'
+WHERE
+goods_code = 'S987' AND store_code = 'EA01'
+https://gyazo.com/491ff983fe2dc93ceb34649863f252ad
  
  
 -- 8. 7で更新した商品を削除しなさい。※実行後のSELECT結果も貼付すること。
--- [回答]
+DELETE FROM stock_table
+WHERE
+goods_code = 'S987' AND store_code = 'EA01'
+https://gyazo.com/4efb31c8b1b865282d093dfae4c861fb
