726453bd73a97095378d755dc7bcb855a63875eb
hfc.3591@gmail.com <85048706+teru-sei@users.noreply.github.com>
2021-12-22 10:33:01 +0900

    Check5　アノテーション記述

--- JavaStudy/demo/src/main/java/com/example/security/springsecurity/account/AccountService.java
+++ JavaStudy/demo/src/main/java/com/example/security/springsecurity/account/AccountService.java
@@ -10,11 +10,11 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 //問３－１ Serviceであることを表すアノテーションを記述
-@
+@Service
 public class AccountService implements UserDetailsService {
 
     //問３－２ 自動でインスタンス生成をするアノテーションを記述
-    @
+    @Autowired
     private AccountRepository repository;
 
     @Autowired
@@ -46,8 +46,8 @@ public class AccountService implements UserDetailsService {
     @Transactional
     public void registerAdmin(String username, String password, String mailAddress) {
         //問３－３ 引数をもとにAccountクラスのインスタンスを生成する構文を記述（passwordはハッシュ化）
-
-        user.setAdmin(true);
+        Account user = new Account(username, passwordEncoder.encode(password), mailAddress);
+        user.setManager(true);
         //userをもとにadmin情報の登録か更新を行う
         repository.save(user);
     }
